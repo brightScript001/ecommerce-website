ee1c5ee980f4d543d59fc9f2aba231bc3c4e195e
Success Bright <bs3webdevelop@gmail.com>
2024-05-28 19:53:48 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-gray-100 py-10 mt-20">
-      <div className="container mx-auto px-4 my-16">
-        <div className="flex flex-col md:flex-row justify-between">
+      <div className="w-full px-[20px] md:px-[100px] lg:px-[100px] my-16">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
           <div className="mb-6 md:mb-0">
             <h3 className="text-4xl font-bold text-gray-800 mb-4">Flone.</h3>
             <p className="text-gray-600">© 2024 Flone. All Rights Reserved</p>
           </div>
-          <div className="w-full md:w-1/4 mb-6 md:mb-0">
-            <h3 className="text-lg font-bold text-gray-800 mb-4">ABOUT US</h3>
+          <div className="w-full md:w-auto mb-6 md:mb-0">
+            <h3 className="text-lg font-medium text-gray-800 mb-4">ABOUT US</h3>
             <ul className="list-none text-gray-600">
               <li>
                 <a href="#" className="hover:text-gray-800">
@@ -34,8 +34,8 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="w-full md:w-1/4 mb-6 md:mb-0">
-            <h3 className="text-lg font-bold text-gray-800 mb-4">
+          <div className="w-full md:w-auto mb-6 md:mb-0">
+            <h3 className="text-lg font-medium text-gray-800 mb-4">
               USEFUL LINKS
             </h3>
             <ul className="list-none text-gray-600">
@@ -61,8 +61,10 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="w-full md:w-1/4 mb-6 md:mb-0">
-            <h3 className="text-lg font-bold text-gray-800 mb-4">FOLLOW US</h3>
+          <div className="w-full md:w-auto mb-6 md:mb-0">
+            <h3 className="text-lg font-medium text-gray-800 mb-4">
+              FOLLOW US
+            </h3>
             <ul className="list-none text-gray-600">
               <li>
                 <a href="#" className="hover:text-gray-800">
@@ -86,8 +88,10 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="w-full md:w-1/4 mb-6 md:mb-0">
-            <h3 className="text-lg font-bold text-gray-800 mb-4">SUBSCRIBE</h3>
+          <div className="w-full md:w-auto mb-6 md:mb-0">
+            <h3 className="text-lg font-medium text-gray-800 mb-4">
+              SUBSCRIBE
+            </h3>
             <p className="text-gray-600 mb-4">
               Get E-mail updates about our latest shop and special offers.
             </p>
